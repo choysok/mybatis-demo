d30375ce03656b5ec02bdea01e1faf01caece760
choysok <56969564+choysok@users.noreply.github.com>
2020-04-19 17:04:25 +0800

    Initial commit

---
+++ src/main/java/com/example/mybatis/bean/User.java
@@ -0,0 +1,40 @@
+package com.example.mybatis.bean;
+
+public class User {
+    private Integer id;
+    private String name;
+    private Integer age;
+    private Double money;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+}
